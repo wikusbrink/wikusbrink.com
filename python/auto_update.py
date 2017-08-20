59b0469119e927bacf79e6353ade40f96e49409b
Wikus Brink <wikus@praelexis.com>
2017-08-20 18:54:36 +0200

    update app with master commit

--- python/auto_update.py
+++ python/auto_update.py
@@ -4,11 +4,19 @@ import json
 app = Flask(__name__)
 
 @app.route('/hook',methods=['POST'])
-def foo():
-   data = json.loads(request.data)
-   print data
-   return "OK"
+def update():
+    data = json.loads(request.data)
+    if data['ref'] == 'refs/heads/master':
+    	try:
+        	os.system('service wikusbrink.com stop')
+        except:
+        	pass
+        os.system('cd /home/ubuntu/wikusbrink.com && git pull')
+        os.system('cd /home/ubuntu/wikusbrink.com/src && npm install')
+        os.system('cd /home/ubuntu/wikusbrink.com/src && bower install')
+        os.system('service wikusbrink.com start')
+
+    return "OK"
 
 if __name__ == '__main__':
-   app.run('0.0.0.0', 8080)
-
+    app.run('0.0.0.0', 8080)
